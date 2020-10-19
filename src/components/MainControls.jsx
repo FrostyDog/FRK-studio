214e2973996765e7d3390737ca836c75d6e46a45
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-10-19 14:36:26 +0200

    feat: complete work comp + spinning text

--- src/components/MainControls.jsx
+++ src/components/MainControls.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import {useState} from "react";
 import { Button, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import RoundText from "../assets/round-text.svg";
+import "../theme/animations.scss";
 
 export default function StableInfo(props) {
   let worksOutlined;
   let bioOutlined;
+  let [RoundTextStance, setRoundTextStance] = useState(false);
+
 
   switch (props.stance) {
     case "works":
       worksOutlined = "outlined";
       bioOutlined = null;
+      setTimeout(() => {
+        setRoundTextStance(true);
+      }, 1200);
       break;
     case "bio":
       bioOutlined = "outlined";
       worksOutlined = null;
+      if(RoundTextStance === true)
+      setRoundTextStance(false)
       break;
     default:
     // code block
@@ -41,6 +51,7 @@ export default function StableInfo(props) {
 
       <div className={`header-info ${props.stance}`}>
         <div class="main-title">
+          {RoundTextStance ? <img src={RoundText} class="round-text" alt="" /> : null}
           <Typography variant="h1" color="primary">
             <span class="bold secondary">a</span>nna <br />
             figura
@@ -53,7 +64,7 @@ export default function StableInfo(props) {
             <span class="bold">FRK design studio</span>
           </Typography>
         </div>
-        <img  className={`arrow ${props.stance}`} src={require("../assets/icons/right-arrow.png")} alt=""/>
+        <img className={`arrow ${props.stance}`} src={require("../assets/icons/right-arrow.png")} alt="" />
       </div>
     </div>
   );
--- src/screens/Works.jsx
+++ src/screens/Works.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import WorksItems from "../components/WorkItems";
 import Mountain from "../assets/workItems/mount-proj.png";
 import Drones from "../assets/workItems/delivery.png";
+import Modernism from "../assets/workItems/pp-modernism.png";
+import CyberSec from "../assets/workItems/syber-sec.png";
+import SushiFreak from "../assets/workItems/sushi-freak.png";
 
 export default function Works(props) {
   return (
@@ -17,7 +20,7 @@ export default function Works(props) {
         and best ones are displayed on the first page 
         under different"
       />
-        <WorksItems
+      <WorksItems
         itemImage={Drones}
         orientation="left"
         title="Drone Delivery"
@@ -26,6 +29,41 @@ export default function Works(props) {
         most popular and useful services. Check out
          this First page made with help of photoshop 
         and some sudden inspiration. "
+      />
+      <WorksItems
+        itemImage={Drones}
+        orientation="left"
+        title="Dr. Martens"
+        text="I was inspired by one of my favorite shoe brands. 
+        Official website looks a bit old, so my idea was
+         to redesign it for more actual and moder"
+      />
+      <WorksItems
+        itemImage={Modernism}
+        orientation="left"
+        title="P.Modernism"
+        text="Web design for a creative event community 
+        group which is making extraordinary and authentic 
+        events. Very interesting and rare kind of service, 
+        honestly, I enjoy the idea as much as I enjoyed"
+      />
+      <WorksItems
+        itemImage={CyberSec}
+        orientation="left"
+        title="Cyber Security"
+        text="The website with full list of services and pricing. 
+        Made with the help of Adobe PS and XD. Main 
+        point here is to show the responsiveness of the 
+        pages and easy UX"
+      />
+      <WorksItems
+        itemImage={SushiFreak}
+        orientation="left"
+        title="Sushi Freak"
+        text="I liked so much working on this project. 
+        Amazing Client and amazing kind of business.
+         This is a concept Sushi restaurant with a big variety
+         of products and services"
       />
     </div>
   );
