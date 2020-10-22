dbcfdd36e0654e539d0ada70b0c7044df6706d05
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-10-22 17:11:08 +0200

    fix: importing Material Styles typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {ThemeProvider} from "@material-ui/core/Styles";
+import {ThemeProvider} from "@material-ui/core/styles";
 import theme from "./theme/theme";
 
 ReactDOM.render(
